164d7a8fc984065fe550ecfba18141e4c4f6a556
Matt Emrick <matt@redtrainwebservices.com>
2026-01-09 10:12:19 -0700

    add buzzer to board; add board test routine

--- src/board.py
+++ src/board.py
@@ -1,10 +1,14 @@
-from gpiozero import LED
+from gpiozero import LED, TonalBuzzer
+from time import sleep
+
+
 class Board:
 
     def __init__(self):
         self.green_led = LED(17)
         self.blue_led = LED(12)
         self.red_led = LED(21)
+        self.buzzer = TonalBuzzer(26, octaves=2)
         self.status_on()
         self.status_disconnected()
 
@@ -13,6 +17,7 @@ class Board:
         self.set_blue(False)
         self.set_green(False)
         self.set_red(False)
+        self.stop_tone()
 
     def status_on(self):
         self.set_green(True)
@@ -43,3 +48,40 @@ class Board:
             self.red_led.on()
         else:
             self.red_led.off()
+
+    def play_tone(self, note="C5"):
+        self.buzzer.play(note)
+
+    def stop_tone(self):
+        self.buzzer.stop()
+
+    def buzz(self, on=True):
+        if (on):
+            self.play_tone('C3')
+        else:
+            self.stop_tone()
+
+
+def board_test():
+    board = Board()
+    board.cleanup()
+    board.buzz()
+    sleep(0.5)
+    board.set_green(True)
+    board.play_tone('C4')
+    sleep(0.5)
+    board.set_blue(True)
+    board.play_tone('E4')
+    sleep(0.5)
+    board.set_red(True)
+    board.play_tone('G4')
+    sleep(0.5)
+    board.stop_tone()
+    sleep(0.5)
+
+
+if __name__ == "__main__":
+    try:
+        board_test()
+    except KeyboardInterrupt:
+        pass
