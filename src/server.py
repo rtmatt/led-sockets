916cfad78c588435269907f40cb154f167399a5b
Matt Emrick <matt@redtrainwebservices.com>
2026-01-11 23:40:45 -0700

    move server stuff to class; annotate handlers

--- src/server.py
+++ src/server.py
@@ -1,137 +1,52 @@
-import json
+import asyncio
 import os
 import signal
+
 from dotenv import load_dotenv
-import asyncio
 from websockets.asyncio.server import serve
 
-load_dotenv()
-
-connections = set()
-
-hardware_connection = None
-
-client_connections = set()
-
-hardware_state = {"on": False, "message": ""}
-
-
-async def echo(websocket):
-    global hardware_connection
-    global hardware_state
-    async for message in websocket:
-        print(message)
-        if (websocket == hardware_connection):
-            payload = json.loads(message)
-            if (payload['type'] == 'hardware_state'):
-                hardware_state = payload['data']
-        for connection in connections:
-            if connection is not websocket:
-                print('sending to connection')
-                await connection.send(message)
-            else:
-                print('skipping origin connection')
-
-
-async def init_client(websocket):
-    global hardware_connection
-    connections.add(websocket)
-    client_connections.add(websocket)
-    print(f"led-sockets server: new client connection ({len(client_connections)} clients connected)")
-    try:
-        payload = {
-            "type": 'client_connection_init',
-            "id": "",
-            "data": {
-                "hardware_is_connected": hardware_connection is not None,
-                "hardware_state": hardware_state
-            }
-        }
-        await websocket.send(json.dumps(payload))
-        await echo(websocket)
-    finally:
-        print('led-sockets server: client disconnected')
-        connections.remove(websocket)
-        client_connections.remove(websocket)
-
-
-async def init_hardware(websocket, payload):
-    global hardware_connection, hardware_state
-    if (hardware_connection is not None):
-        print('led-sockets server: hardware already connected. Ignoring')
-        return
-    connections.add(websocket)
-    hardware_connection = websocket
-    assert payload['data']['hardware_state']
-    hardware_state = payload['data']['hardware_state']
-    print(f"led-sockets server: new hardware connection")
-    try:
-        for connection in client_connections:
-            print('led-sockets server: sending hardware connection to clients')
-            payload = {
-                "type": "hardware_connection",
-                "id": "",
-                "data": {
-                    "is_connected": True,
-                    "state": hardware_state
-                }
-            }
-            await connection.send(json.dumps(payload))
-
-        await echo(websocket)
-    finally:
-        print('led-sockets server: hardware disconnected')
-        connections.remove(websocket)
-        hardware_connection = None
-        hardware_state = {"on": False}
-        for connection in client_connections:
-            print('led-sockets server: sending hardware disconnection to clients')
-            payload = {
-                "type": "hardware_connection",
-                "id": "",
-                "data": {
-                    "is_connected": False,
-                    "state": hardware_state
-                }
-            }
-            await connection.send(json.dumps(payload))
-
-
-async def handler(websocket):
-    message = await websocket.recv()
-    payload = json.loads(message)
-    assert payload['type'] == 'init'
-    if (payload["data"]["entity_type"] == 'hardware'):
-        await init_hardware(websocket, payload)
-    elif (payload["data"]["entity_type"] == 'client'):
-        await init_client(websocket)
+from EchoServer import EchoServer
 
 
 async def setup():
+    """
+    Set up the server
+    Opens a websockets server with EchoServer.init_socket as the handler
+    """
     host = os.getenv('ECHO_SERVER_HOST', '0.0.0.0')
     port = os.getenv('ECHO_SERVER_PORT', '8765')
     print(f"led-sockets server: starting on {host}:{port}")
+    echo_server = EchoServer()
     try:
         async with serve(
-                handler,
+                echo_server.handle_connection,
                 host,
                 int(port)
-        ) as server:
-            await server.serve_forever()
+        ) as echo_server:
+            await echo_server.serve_forever()
     except asyncio.CancelledError:
         pass
 
 
 async def shutdown(sig, tasks):
+    """
+    :param sig: the shutdown signal
+    :param tasks: the tasks to cancel
+    Shut down the server.  Cancel all tasks
+    """
     print(f"led-sockets server: shutting down ({sig.name})...")
     for task in tasks:
         task.cancel()
 
     await asyncio.gather(*tasks, return_exceptions=True)
-    print("Done.")
 
 
 async def main():
+    """
+    Coordinate application lifecycle
+    Wrap setup within asyncio task which can be canceled when SIGINT or SIGTERM are heard
+    This ensures the application can be stopped without error when monitored by something like supervisord
+    """
     loop = asyncio.get_running_loop()
     tasks = [
         asyncio.create_task(setup())
@@ -143,5 +58,6 @@ async def main():
 
 
 if __name__ == "__main__":
+    load_dotenv()
     print(f"led-sockets server: starting pid {os.getpid()}")
     asyncio.run(main())
