cd4b7a80e2c4662479f3dc127abd1a058e6e968b
Matt Emrick <matt@redtrainwebservices.com>
2026-01-13 14:10:13 -0700

    refactor ClientManager to restructure connection flow and enhance shutdown handling

--- src/ClientManager.py
+++ src/ClientManager.py
@@ -3,9 +3,10 @@ import datetime
 import json
 import os
 import signal
+from typing import Union
 
 from dotenv import load_dotenv
-from websockets.asyncio.client import connect
+from websockets.asyncio.client import connect, ClientConnection
 
 
 # # @TODO:
@@ -13,68 +14,110 @@ from websockets.asyncio.client import connect
 
 class ClientManager:
     LOG_PREFIX = 'led-sockets-client'
+    CONNECTION_CLOSING_MESSAGE = 'I am dying'
+    _connection: Union[None | ClientConnection]
+    _host_url: str
 
     def __init__(self, host_url):
         self._host_url = host_url
+        self._connection = None
+        self._tasks = []
 
     def _log(self, msg):
         timestamp = datetime.datetime.now().isoformat()
         print(f"{self.LOG_PREFIX} [{timestamp}] {msg}")
 
-    async def _create_connection(self):
+    async def _listen(self):
+        """
+        Listen to the websocket connection for new messages
+        """
+        self._log('Initialized.  Listening for messages...')
+        if self._connection:
+            async for message in self._connection:
+                self._log(f'Server says: {message}  {self._connection.remote_address}')
+        else:
+            raise Exception('Listen called without connection')
+
+    async def handle_connection_closed(self):
+        self._log('Server connection closed')
+
+    async def _initialize_connection(self):
+        """
+        Initialize websocket connection with identification payload
+        """
+        if not self._connection:
+            raise Exception('Initializing non-existent connection')
+
+        self._log('Initializing Connection')
+        payload = {
+            "type": "init_hardware",
+            "relationships": {
+                "hardware_state": {
+                    "data": self._get_hardware_state()
+                }
+            }
+        }
+        await self._connection.send(json.dumps(payload))
+
+    def _get_hardware_state(self):
+        return {
+            "type": "hardware_state",
+            "attributes": {
+                "on": False  # @todo: get state from board
+            }
+        }
+
+    async def _build_connection(self):
+        """
+        Build the websocket connection and queue initialization/listening
+        """
         try:
             self._log('Establishing connection')
             async with connect(self._host_url) as websocket:
                 self._log('Connected')
                 self._connection = websocket
-
-                self._log('Sending init payload')
-                payload = {
-                    "type": "init_hardware",
-                    "relationships": {
-                        "hardware_state": {
-                            "data": {
-                                "type": "hardware_state",
-                                "attributes": {
-                                    "on": False  # @todo: get state from board
-                                }
-                            }
-                        }
-                    }
-                }
-                await self._connection.send(json.dumps(payload))
-
-                self._log('Initialized.  Listening for messages...')
-                async for message in self._connection:
-                    self._log(f'Server says: {message}  {self._connection.remote_address}')
-
-                self._log('Server connection closed')
+                await self._initialize_connection()
+                await self._listen()
+                await self.handle_connection_closed()
         except OSError as e:
             self._log(f"Connection failed: {e}")
         except asyncio.CancelledError:
-            print('cancel error caught')
+            pass
         finally:
             self._connection = None
 
-    async def shutdown(self, sig, tasks):
+    async def _shutdown(self, sig):
+        """
+        Perform a graceful shutdown.
+        Cancel all active tasks within loop and notify connections of shutdown
+        :param sig: The signal triggering the shutdown (SIGINT, SIGTERM
+        :param tasks: The tasks to cancel for the shutdown
+        """
         self._log(f"Shutdown signal received ({sig.name})")
         if (self._connection):
             self._log('Broadcasting impending death')
-            await self._connection.send('I am dying')
+            await self._connection.send(self.CONNECTION_CLOSING_MESSAGE)
             await self._connection.close()
-        for task in tasks:
+        for task in self._tasks:
             task.cancel()
-        await asyncio.gather(*tasks, return_exceptions=True)
+        # @todo should not need to gather since it's already done in start_server
+        # await asyncio.gather(*self._tasks, return_exceptions=True)
 
     async def _start_server(self):
+        """
+        Create task wrapper for calling connection creation method
+        Allows process to terminate gracefully on SIGINT and SIGKILL
+
+        Registers shutdown handler upon signal
+        """
         self._log(f"Setting up context")
         loop = asyncio.get_running_loop()
-        tasks = [
-            asyncio.create_task(self._create_connection())
+        self._tasks = [
+            asyncio.create_task(self._build_connection())
         ]
         for sig in (signal.SIGINT, signal.SIGTERM):
-            loop.add_signal_handler(sig, lambda sig=sig: asyncio.create_task(self.shutdown(sig, tasks)))
-        await asyncio.gather(*tasks)
+            loop.add_signal_handler(sig, lambda sig=sig: asyncio.create_task(self._shutdown(sig)))
+        await asyncio.gather(*self._tasks)
 
     def serve(self):
         self._log(f"Starting (pid {os.getpid()})")
