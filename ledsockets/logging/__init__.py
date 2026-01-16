67a394a8819628967949779908723b21f8c49eb7
Matt Emrick <matt@redtrainwebservices.com>
2026-01-16 08:59:45 -0700

    expand logging configuration to include additional files and dedicated loggers for board and general messages

--- ledsockets/logging/__init__.py
+++ ledsockets/logging/__init__.py
@@ -4,8 +4,7 @@ from pathlib import Path
 
 current_file_path = Path(__file__)
 target_dir = current_file_path.parent.parent.parent
-target_file_path = target_dir / "logs" / "ledsockets-server.log"
-target_file_path2 = target_dir / "logs" / "ledsockets-client.log"
+target_dirpath = target_dir / "logs"
 
 LOGGING = {
     "version": 1,
@@ -23,25 +22,45 @@ LOGGING = {
         },
         "file": {
             "class": "logging.handlers.RotatingFileHandler",
-            "filename": target_file_path,
+            "filename": target_dirpath / "ledsockets-server.log",
             "formatter": "cust",
             "maxBytes": 1000000,
             "backupCount": 5
         },
         "file2": {
             "class": "logging.handlers.RotatingFileHandler",
-            "filename": target_file_path2,
+            "filename": target_dirpath / "ledsockets-client.log",
+            "formatter": "cust",
+            "maxBytes": 1000000,
+            "backupCount": 5
+        },
+        "file3": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": target_dirpath / "ledsockets-board.log",
+            "formatter": "cust",
+            "maxBytes": 1000000,
+            "backupCount": 5
+        },
+        "file4": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": target_dirpath / "ledsockets.log",
             "formatter": "cust",
             "maxBytes": 1000000,
             "backupCount": 5
         }
     },
     "loggers": {
+        "ledsockets": {
+            "handlers": ["stdout", "file4"], "level": "DEBUG"
+        },
+        "ledsockets.board": {
+            "handlers": ["file3"], "level": "DEBUG"
+        },
         "ledsockets.server": {
-            "handlers": ["stdout", "file"], "level": "DEBUG"
+            "handlers": ["file"], "level": "DEBUG"
         },
         "ledsockets.client": {
-            "handlers": ["stdout", "file2"], "level": "DEBUG"
+            "handlers": ["file2"], "level": "DEBUG"
         }
     },
 }
