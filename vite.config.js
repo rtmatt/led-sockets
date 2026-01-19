22248ac66759189126303d1c1126f7c57f2f97f3
Matt Emrick <matt@redtrainwebservices.com>
2026-01-18 20:59:57 -0700

    CHANGE: move `index.html` entrypoint into `src` directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
+import path from 'node:path';
+import process from 'node:process';
 
 export default defineConfig({
-  build:{
-    outDir: 'dist'
+  build: {
+    outDir: '../dist'
+  },
+  root: 'src',
+  publicDir: '../public',
+  resolve: {
+    alias: { '/src': path.resolve(process.cwd(), 'src') }
   }
 })
