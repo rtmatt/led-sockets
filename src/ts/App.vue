ffe314f1817b64e42b44eef658295cf4202a034b
Matt Emrick <matt@redtrainwebservices.com>
2026-01-20 17:28:37 -0700

    FEAT: introduce Vue 3-based frontend setup with TypeScript
    - Add base Vue 3 app with TypeScript support (`App.vue`, `main.ts`).
    - Configure Vite for module aliasing and Vue DevTools plugin.
    - Add TypeScript configurations (`tsconfig.*.json`) for node and app contexts.
    - Include ESLint setup with Vue and TypeScript integration.
    - Update `index.html` to integrate Vue app entry.
    - Add necessary dependencies (`vue`, `vite-plugin-vue-devtools`, `vue-tsc`).

---
+++ src/ts/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const message = ref('World');
+</script>
+
+<template>
+  <div>
+    Hello {{message}}
+  </div>
+</template>
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import path from 'node:path';
 import process from 'node:process';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
 export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools()
+  ],
   build: {
     outDir: '../dist'
   },
   root: 'src',
-  envDir:'../',
+  envDir: '../',
   publicDir: '../public',
   resolve: {
-    alias: { '/src': path.resolve(process.cwd(), 'src') }
+    alias: {
+      '/src': path.resolve(process.cwd(), 'src'),
+      '@': fileURLToPath(new URL('./src/ts', import.meta.url))
+    }
   }
-})
+});
