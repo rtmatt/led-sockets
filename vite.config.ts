2be5570217d7e244674c0d1092931166eac05be3
Matt Emrick <matt@redtrainwebservices.com>
2026-01-22 11:14:35 -0700

    sync with integration dev

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools';
+import { fileURLToPath, URL } from 'node:url';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueDevTools()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src/ts', import.meta.url))
+    }
+  }
+})
