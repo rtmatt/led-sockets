c5cec6a7b89f59374a7c043b91a1396f7e2aae55
Matt Emrick <matt@redtrainwebservices.com>
2026-02-02 13:51:59 -0700

    FEAT: broadcast `UiMessage` events on client connect/disconnect in `ServerConnectionManager`

--- src/ledsockets/server/ServerConnectionManager.py
+++ src/ledsockets/server/ServerConnectionManager.py
@@ -13,6 +13,7 @@ from ledsockets.dto.PartialHardwareState import PartialHardwareState
 from ledsockets.dto.ServerStatus import ServerStatus
 from ledsockets.dto.TalkbackMessage import TalkbackMessage
 from ledsockets.dto.UiClient import UiClient
+from ledsockets.dto.UiMessage import UiMessage
 from ledsockets.log.LogsConcern import Logs
 from ledsockets.support.Message import Message, MessageException
 
@@ -76,6 +77,17 @@ class ServerConnectionManager(Logs, AbstractServerConnectionManager):
         if client_id and client_id in self._client_connections:
             del self._client_connections[client_id]
 
+        obj = self._get_status()
+        obj.set_relationship('ui_client', client)
+        obj.set_relationship('ui_message', UiMessage(f"{client.name} left"))
+
+        await self._broadcast_to_clients(json.dumps([
+            'client_disconnect',
+            {
+                "data": obj.toDict()
+            }
+        ]), exclude_ids=[client.id])
+
     async def _on_talkback_message(self, message: Message, source: str):
         try:
             talkback = TalkbackMessage.from_message(message)
@@ -130,6 +142,7 @@ class ServerConnectionManager(Logs, AbstractServerConnectionManager):
         result_obj = self._get_status()
         result_obj.set_relationship('ui_client', client)
         result_obj.append_relationship('talkback_messages', TalkbackMessage("Hello, client!"))
+        result_obj.set_relationship('ui_message', UiMessage(f'You joined'))
         await client.connection.send(json.dumps([
             'client_init',
             {
@@ -137,6 +150,7 @@ class ServerConnectionManager(Logs, AbstractServerConnectionManager):
             }
         ]))
         result_obj.remove_relationship('talkback_messages')
+        result_obj.set_relationship('ui_message', UiMessage(f'Client {client.name} joined'))
         await self._broadcast_to_clients(json.dumps([
             'client_joined',
             {
