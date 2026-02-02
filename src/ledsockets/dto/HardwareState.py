4849a8813132d63993b295c3d7df575575613dfd
Matt Emrick <matt@redtrainwebservices.com>
2026-02-02 15:59:59 -0700

    ADD: add `source` relation/member to `HardwareState`
    - ADD: `source` via computed getter/setter to define member variable and relation
    - import source relations within `from_dict` method
    
    TEST: add `test_PartialHardwareState` test

--- src/ledsockets/dto/HardwareState.py
+++ src/ledsockets/dto/HardwareState.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 from ledsockets.dto.AbstractDto import AbstractDto
+from ledsockets.dto.UiClient import UiClient
 
 
 class HardwareState(AbstractDto):
@@ -10,6 +11,19 @@ class HardwareState(AbstractDto):
         super().__init__(id)
         self.on = on
         self.status_description = status_description
+        self._source = None
+
+    @property
+    def source(self):
+        return self._source
+
+    @source.setter
+    def source(self, val: UiClient | None):
+        self._source = val
+        if (val):
+            self.set_relationship('source', val)
+        else:
+            self.remove_relationship('source')
 
     def get_attributes(self):
         return {
@@ -19,6 +33,20 @@ class HardwareState(AbstractDto):
 
     def copy(self):
         return HardwareState(self.on, self.status_description)
+
+    @classmethod
+    def from_dict(self, json_data: Dict):
+        instance = super().from_dict(json_data)
+        relationships = json_data.get('relationships')
+        if (relationships):
+            source_relationship = relationships.get('source')
+            source_relation_data = source_relationship.get('data') if source_relationship else None
+            if (source_relation_data):
+                if (source_relation_data.get('type') == 'ui_client'):
+                    client = UiClient.from_dict(source_relation_data)
+                    instance.source = client
+
+        return instance
 
     @classmethod
     def _inst_from_attributes(cls, attributes: Dict, id: str = ''):
