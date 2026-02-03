98987ba659ead3ef4e866ebd4f55f36b5af9fdf9
Matt Emrick <matt@redtrainwebservices.com>
2026-02-03 14:50:54 -0700

    FEAT: add retention of client name on reload/reconnect
    - ADD: preserve existing client name when available upon establishing a connection in `ServerConnectionManager`
    - ADD: persist and restore `ui_client` connection from localStorage in `App.vue`

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -110,6 +110,7 @@ function openConnection() {
         data: {
           id: '',
           type: 'ui_client',
+          attributes: client.value ? client.value.attributes : {},
         },
       },
     ];
@@ -165,6 +166,7 @@ function openConnection() {
           isHardwareConnected.value = payload.attributes.hardware_is_connected;
           if (payload.relationships.ui_client) {
             client.value = payload.relationships.ui_client.data;
+            localStorage.setItem('ledsockets.connection', JSON.stringify(payload.relationships.ui_client.data));
             const { name } = payload.relationships.ui_client.data.attributes;
             addMessage({
               message: `You joined.  Your name is ${name}.`,
@@ -289,6 +291,14 @@ function addMessage(message: UiMessageAttributes) {
 }
 
 onMounted(() => {
+  const storedClient = localStorage.getItem('ledsockets.connection');
+  if (storedClient) {
+    const previousConnection = JSON.parse(storedClient);
+    if (previousConnection) {
+      client.value = previousConnection;
+    }
+  }
+
   connect();
 });
 const hardwareStatus = computed(() => {
