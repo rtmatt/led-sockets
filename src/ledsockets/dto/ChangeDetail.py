53d64bdfd96194a0adedfb617733cbd5b89bbb0b
Matt Emrick <matt@redtrainwebservices.com>
2026-02-02 20:36:54 -0700

    ADD: `ChangeDetail` DTO

---
+++ src/ledsockets/dto/ChangeDetail.py
@@ -0,0 +1,34 @@
+from typing import Dict
+
+from ledsockets.dto.AbstractDto import AbstractDto
+
+
+class ChangeDetail(AbstractDto):
+    TYPE = 'change_detail'
+
+    def __init__(self, description='', source_name='', action_description='', source_type='', source_id='', id=''):
+        super().__init__(id)
+        self.description = description
+        self.source_name = source_name
+        self.action_description = action_description
+        self.source_type = source_type
+        self.source_id = source_id
+
+    def get_attributes(self):
+        return {
+            "description": self.description,
+            "source_name": self.source_name,
+            "action_description": self.action_description,
+            "source_type": self.source_type,
+            "source_id": self.source_id,
+        }
+
+    @classmethod
+    def _inst_from_attributes(cls, attributes: Dict, id: str = ''):
+        return cls(
+            attributes.get('description'),
+            attributes.get('source_name'),
+            attributes.get('action_description'),
+            attributes.get('source_type'),
+            attributes.get('source_id'),
+        )
