62aaac7e5ff0b7046c90a79fc0d908ebc719ebf7
Matt Emrick <matt@redtrainwebservices.com>
2026-02-03 14:50:54 -0700

    ADD: persist and restore `ui_client` connection from localStorage in `App.vue`

--- src/ts/App.vue
+++ src/ts/App.vue
@@ -165,6 +165,7 @@ function openConnection() {
           isHardwareConnected.value = payload.attributes.hardware_is_connected;
           if (payload.relationships.ui_client) {
             client.value = payload.relationships.ui_client.data;
+            localStorage.setItem('ledsockets.connection', JSON.stringify(payload.relationships.ui_client.data));
             const { name } = payload.relationships.ui_client.data.attributes;
             addMessage({
               message: `You joined.  Your name is ${name}.`,
@@ -289,6 +290,14 @@ function addMessage(message: UiMessageAttributes) {
 }
 
 onMounted(() => {
+  const storedClient = localStorage.getItem('ledsockets.connection');
+  if (storedClient) {
+    const previousConnection = JSON.parse(storedClient);
+    if (previousConnection) {
+      client.value = previousConnection;
+    }
+  }
+
   connect();
 });
 const hardwareStatus = computed(() => {
