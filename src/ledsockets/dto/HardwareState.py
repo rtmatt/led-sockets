1ca4bc39c55cd6af47fae1397f8366044186b356
Matt Emrick <matt@redtrainwebservices.com>
2026-02-02 20:36:54 -0700

    ADD: Add change detail information to hardware updated messages
      - ADD: `ChangeDetail` DTO
      - ADD: `change_detail` relationship to `HardwareState` DTO
      - ADD: `ChangeDetail` relation information into hardware state change handling

--- src/ledsockets/dto/HardwareState.py
+++ src/ledsockets/dto/HardwareState.py
@@ -1,6 +1,7 @@
 from typing import Dict
 
 from ledsockets.dto.AbstractDto import AbstractDto
+from ledsockets.dto.ChangeDetail import ChangeDetail
 from ledsockets.dto.UiClient import UiClient
 
 
@@ -12,6 +13,7 @@ class HardwareState(AbstractDto):
         self.on = on
         self.status_description = status_description
         self._source = None
+        self._change_detail = None
 
     @property
     def source(self):
@@ -24,6 +26,20 @@ class HardwareState(AbstractDto):
             self.set_relationship('source', val)
         else:
             self.remove_relationship('source')
+
+    @property
+    def change_detail(self):
+        return self._change_detail
+
+    @change_detail.setter
+    def change_detail(self, val: ChangeDetail | None):
+        if (not isinstance(val, ChangeDetail)):
+            raise TypeError("Invalid change_detail")
+        self._change_detail = val
+        if (val):
+            self.set_relationship('change_detail', val)
+        else:
+            self.remove_relationship('change_detail')
 
     def get_attributes(self):
         return {
@@ -45,6 +61,13 @@ class HardwareState(AbstractDto):
                 if (source_relation_data.get('type') == 'ui_client'):
                     client = UiClient.from_dict(source_relation_data)
                     instance.source = client
+
+            change_relation = relationships.get('change_detail')
+            change_relation_data = change_relation.get('data') if change_relation else None
+            if (change_relation_data):
+                if (change_relation_data.get('type') == 'change_detail'):
+                    detail = ChangeDetail.from_dict(change_relation_data)
+                    instance.change_detail = detail
 
         return instance
 
